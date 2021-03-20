34e1a2d0a3a8f1dfeae63de0ed8faf03085ca73e
florentdrilhon <florentdrilhonvvs@gmail.com>
2021-03-20 16:50:48 +0100

    Map ameliorated & first component added on the map

--- client/src/App.js
+++ client/src/App.js
@@ -13,10 +13,11 @@ require('dotenv').config();
 const historicalBuildingTest={
   _id:"123",
   name: "Super immeuble qui tue sa mère",
+  latitude:48.850853542905114,
+  longitude:2.343891862961394,
   address: "12 Boulevard du Général de Gaule",
   city : "Paris",
   description : "Un immeuble ultra boosté qui tue sa mère"
-
 }
 
 
@@ -30,17 +31,17 @@ const historicalBuildingTest={
 class App extends Component {
 
   render () {
-    const {_id, name, address, city, description}= historicalBuildingTest
+    const myBuilding= historicalBuildingTest;
     return (
       <div>
           <HistoricalBuilding 
-              _id={_id}
-              name={name}
-              address={address}
-              city={city}
-              description={description}
+              _id={myBuilding._id}
+              name={myBuilding.name}
+              address={myBuilding.address}
+              city={myBuilding.city}
+              description={myBuilding.description}
           />
-          < MapContainer/>
+          < MapContainer historicalBuilding= {myBuilding}/>
       </div>
     ) 
   }
--- client/src/components/MapContainer/MapContainer.js
+++ client/src/components/MapContainer/MapContainer.js
@@ -1,27 +1,65 @@
+import './MapContainer.css';
+
 import * as React from 'react';
 import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 require('dotenv').config()
 
 
-const REACT_APP_MAPBOX_TOKEN="pk.eyJ1IjoiZmxvcmVudGRyaWxoaG9uIiwiYSI6ImNrbWh0YnZoZjBhbTkzMXMxZzBmNzRpODYifQ.pKLn68tATvgzJyVDjGC1dQ"
 
-function MapContainer() {
-  const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 48.856614,
-    longitude:2.3522219,
-    zoom: 8
-  });
+function MapContainer({ historicalBuilding }) {
+    const [viewport, setViewport] = useState({
+        width: "100vw",
+        height: "100vh",
+        latitude: 48.856614,
+        longitude: 2.3522219,
+        zoom: 10
+    });
 
-  return (
-    <ReactMapGL
-      {...viewport}
-      mapboxApiAccessToken={REACT_APP_MAPBOX_TOKEN}
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
-  );
+    const [selectedHistoricalMonument, setSelectedHistoricalMonument] = useState(null)
+
+    return (
+        <div>
+            <ReactMapGL
+                {...viewport}
+                mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+                mapStyle="mapbox://styles/mapbox/streets-v11"
+                onViewportChange={nextViewport => setViewport(nextViewport)}
+            >
+                
+                    <Marker
+                        key={historicalBuilding._id}
+                        latitude={historicalBuilding.latitude}
+                        longitude={historicalBuilding.longitude}
+                    >
+                     <button 
+                     className="marker-btn"
+                     onClick={event => {
+                         event.preventDefault();
+                         setSelectedHistoricalMonument(historicalBuilding)
+                     }}
+                     >
+                        <img src="/monument.svg" alt="Monument Icon" />
+                            
+                    </button>
+                    </Marker>
+                
+
+                {selectedHistoricalMonument ? (
+                    <Popup 
+                     latitude = {selectedHistoricalMonument.latitude}
+                     longitude= {selectedHistoricalMonument.longitude}>
+                        <div>
+                            {selectedHistoricalMonument.description}
+                        </div>
+                    </Popup>
+                ): null}
+
+            </ReactMapGL>
+
+        </div>
+
+    );
 }
 
 export default MapContainer;
