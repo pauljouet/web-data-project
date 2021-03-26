62678cf0b0646704db9c265d71cec82a41b6ba2c
florentdrilhon <florentdrilhonvvs@gmail.com>
2021-03-26 14:08:07 +0100

    queries added in API

--- server/app.py
+++ server/app.py
@@ -5,10 +5,9 @@ from flask_cors import CORS
 from flask_restful import Resource, Api, reqparse
 from fuseki_managements.bikeStation import getStationsData
 from fuseki_managements.monument import getMonumentData
+from fuseki_managements.museum import getMuseumData
 from fuseki_managements.manage_fuseki import deleteDefaultGraph
-# import functions to get stations
-# import functions to get museums
-# import functions to get monument
+
 
 
 app=Flask(__name__)
@@ -34,7 +33,7 @@ def stations():
 # endpoint to get museums
 @app.route('/api/museums', methods=['GET'])
 def museums():
-    museums= functionGetMuseums()
+    museums= getMuseumData()
     return jsonify(museums)
 
 # endpoint to get monuments
@@ -43,12 +42,6 @@ def monuments():
     monuments=getMonumentData()
     return jsonify(monuments)
 
-
-# endpoint to get monuments
-@app.route('/api/deleteAll', methods=['GET'])
-def monuments():
-    rep=deleteDefaultGraph()
-    return jsonify(rep)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
